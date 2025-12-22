ba5dbd8a905fbfb3f3e05619e32611cc9cc84f72
yeshengAust <3220244679@qq.com>
2025-12-22 16:38:28 +0800

    Delete target/classes directory

--- target/classes/sql/migration_add_modification_type.sql
+++
@@ -1,9 +0,0 @@
--- 迁移脚本：为task表添加modification_type字段
--- 执行时间：2025-12-22
-
-USE `kfc_aigc`;
-
--- 检查并添加 modification_type 字段
-ALTER TABLE `task` 
-ADD COLUMN `modification_type` VARCHAR(100) DEFAULT NULL COMMENT '修改类型（删除/新增/修改/替换/加回等，多个类型用逗号分隔）' 
-AFTER `modification_points`;
